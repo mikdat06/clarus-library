143268abb7420d7d4ecc8d7067053782cf26055c
mikdat <mikdatarmutcu123@gmail.com>
2023-08-02 17:29:47 +0300

    first commit

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import {
+  FormContainer,
+  Header,
+  LoginContainer,
+  StyledButton,
+  StyledForm,
+  StyledInput,
+} from "./Login.style";
+import { useAuthContext } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const { setUser } = useAuthContext();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUser(true);
+    navigate(-1);
+  };
+
+  return (
+    <LoginContainer>
+      <FormContainer>
+        <StyledForm onSubmit={handleSubmit}>
+          <Header>Login Here</Header>
+          <StyledInput type="text" placeholder="Username" required />
+          <StyledInput type="password" placeholder="password" required />
+          <StyledButton type="submit">Login</StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
+  );
+};
+
+export default Login;
